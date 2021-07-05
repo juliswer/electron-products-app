f85991a6f052881c73d26a2655581b743654b219
Julián Swerdlin <julianswer36@gmail.com>
2021-07-04 22:06:10 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ const path = require('path');
 if(process.env.NODE_ENV !== 'production'){
     require('electron-reload')(__dirname, {
         electron: path.join(__dirname, '../node_modules', '.bin', '.electron')
-    })
-}
+    });
+};
 
 let mainWindow;
 let newProductWindow;
